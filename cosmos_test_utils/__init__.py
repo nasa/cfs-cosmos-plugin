03d124c8fc9fd8a0b13bd977c6107a9b65fb7855
Damon Stewart <Damon.Stewart@nasa.gov>
2025-09-26 10:14:32 -0400

    #14 Update version to only edit 1 place

--- cosmos_test_utils/__init__.py
+++ cosmos_test_utils/__init__.py
@@ -61,5 +61,14 @@ from .timing_utils import (
 
 from .command_utils import CommandSender
 
-# Define the version
-__version__ = '1.0.1'
+# Pull the version from pyproject so there is only one place to maintain
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError  # For Python <3.8    # type: ignore 
+
+try:
+    __version__ = version("test_utils")
+except PackageNotFoundError:
+    __version__ = "unknown"
+    
