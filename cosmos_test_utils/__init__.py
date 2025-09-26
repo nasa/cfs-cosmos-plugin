3fc9329392cb369b1566fa0bf7d2a6d13e89b017
Damon Stewart <Damon.Stewart@nasa.gov>
2025-09-26 10:39:17 -0400

    #14 There has to be 2 places for the version number due to variety of project use.

--- cosmos_test_utils/__init__.py
+++ cosmos_test_utils/__init__.py
@@ -61,14 +61,5 @@ from .timing_utils import (
 
 from .command_utils import CommandSender
 
-# Pull the version from pyproject so there is only one place to maintain
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    from importlib_metadata import version, PackageNotFoundError  # For Python <3.8    # type: ignore 
-
-try:
-    __version__ = version("test_utils")
-except PackageNotFoundError:
-    __version__ = "unknown"
-    
+# Define the version
+__version__ = '1.0.2'
