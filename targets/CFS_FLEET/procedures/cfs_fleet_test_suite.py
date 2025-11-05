1b3b625667437b20a9040aee9eb233092af9c32b
krmoore2 <keegan.r.moore@nasa.gov>
2025-11-05 16:16:27 -0500

    Remove ports from plugin.txt and add global_tlm_output_ip

--- targets/CFS_FLEET/procedures/cfs_fleet_test_suite.py
+++ targets/CFS_FLEET/procedures/cfs_fleet_test_suite.py
@@ -24,18 +24,9 @@ class cfs_fleet_test_suite(Suite):
         # Create a list of fsw targets
         fleet = CFS_Fleet()
         fsw_targets_to_test = fleet.get_cfs_fleet_target_name_list()
-        # num_cfs_targets = <%= $cfs_fleet_globals_num_cfs_targets %>
-        # Group.print(f"Starting cFS Fleet Test Suite against {num_cfs_targets} target(s).")
-        # fsw_targets_to_test = []
-        # for cpu_index in range(num_cfs_targets):
-        #     cfs_target_name = f"CFS-{cpu_index + 1}"
-        #     fsw_targets_to_test.append(cfs_target_name)
-        #     Group.print(f"cFS Target Name: {cfs_target_name}")
 
         # Determine the correct IP address for FSW to send telemetry to
-        # cosmos_ip_addr = ask("Enter the COSMOS IP address (that FSW nodes should send telemetry to):",
-        #                     "127.0.0.1", False)
-        cosmos_ip_addr = "127.0.0.1"
+        cosmos_ip_addr = <%= global_tlm_output_ip %>
 
         # Enable Telemetry for all FSW nodes
         for fsw_target in fsw_targets_to_test:
